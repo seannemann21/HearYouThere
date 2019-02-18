216b7929e19eff90f9eb90b6c098dd51889c86bc
Sean Nemann <nemann.3@buckeyemail.osu.edu>
2019-02-17 23:17:58 -0500

    Added main page and artist generator components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ArtistGenerator extends React.Component{
+
+	constructor(props) {
+		super(props);
+
+		this.state = {imageUrl:""}
+		this.updateImage = this.updateImage.bind(this);
+	}
+
+	updateImage(event) {
+		const imageUrl = event.target.value;
+		this.setState({imageUrl: imageUrl});
+	}
+
+	render() {
+		return (
+			<div>
+				<img className="festival-image" src={this.state.imageUrl}/>
+				<input type="text" value={this.state.imageUrl} onChange={this.updateImage}/>
+			</div>
+		)
+	}
+}
+
+class MainPage extends React.Component{
+	render() {
+		return(
+			<div>
+				<ArtistGenerator/>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<MainPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
